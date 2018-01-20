1ef8f6a8e623d4f5b5bcd7c7331ab1e8f34f8998
Ren Wang <renwang435@gmail.com>
2018-01-19 22:44:04 -0800

    Working Softmax

--- IMP_CNN/CNN_Inf.py
+++ IMP_CNN/CNN_Inf.py
@@ -1,20 +1,30 @@
 import os
+import argparse
 import tensorflow as tf
 import numpy as np
 import cv2
 
 import CNN_Arch
 
-#Relative Path to meta file
-#Relative Path to image
-#Relative Path to pre-trained weights
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--meta_path', type=str, default='./batches.meta.txt',
+                    help='Path to txt file containing a class on each line')
+
+parser.add_argument('--img_path', type=str, default='./test.jpg',
+                    help='Path to image to run inference on')
+
+parser.add_argument('--train_dir', type=str, default='./CNN_Train',
+                    help='Path to directory with pre-trained weights')
+
+infFLAGS = parser.parse_args()
 
 def restore_vars(saver, sess, chkpt_dir):
     """ Restore saved net, global score and step, and epsilons OR
     create checkpoint directory for later storage. """
     sess.run(tf.global_variables_initializer())
 
-    checkpoint_dir = chkpt_dir
+    checkpoint_dir = infFLAGS.train_dir
 
     if not os.path.exists(checkpoint_dir):
         try:
@@ -29,12 +39,22 @@ def restore_vars(saver, sess, chkpt_dir):
         saver.restore(sess, path.model_checkpoint_path)
         return True
 
+def load_meta():
+    d = {}
+    with open(infFLAGS.meta_path) as f:
+        i = 0
+        for line in f:
+            d[i] = line
+            i = i + 1
+
+    return d
+
 if __name__ == '__main__':
     to_restore = True
     with tf.Graph().as_default():
 
         # Gets the image
-        images = cv2.imread('./test2.jpg')
+        images = cv2.imread(infFLAGS.img_path)
 
         images = np.asarray(images, dtype=np.float32)
         images = tf.convert_to_tensor(images / 255.0)
@@ -53,15 +73,16 @@ if __name__ == '__main__':
 
         with tf.Session() as sess:
             if to_restore:
-                restored = restore_vars(saver, sess, './CNN_Train')
+                restored = restore_vars(saver, sess, infFLAGS.train_dir)
                 to_restore = False
 
             _, top_k_pred = tf.nn.top_k(logits, k=5)
             top_indices = sess.run([top_k_pred])
+            top_pred = top_indices[0][0][0]
+            dict = load_meta()
 
-            print("Predicted ", top_indices[0], " for your input image.")
-            # logit_val = sess.run(logits)
-            # print(logit_val)
+            print("Predicted ", dict[top_pred], " for your input image.")
+            #print(infFLAGS.train_dir)
 
 
 
